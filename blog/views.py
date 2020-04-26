6a7da4426b56f9c9b05031b1fc5a552fb2228d9a
shahilkp6 <shahilkp6@gmail.com>
2020-04-26 06:33:02 +0000

    that

--- blog/views.py
+++ blog/views.py
@@ -8,10 +8,10 @@ from django.utils import timezone
 # Create your views here
 #
 def post_list(request):
-<<<<<<< HEAD
-     return render(request, 'blog/post_list.html', {})
-=======
+
+
+
      posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
      return render(request, 'blog/post_list.html', {'posts':posts})
->>>>>>> f4da04642f4a80f03f0b3c2985c655d587ddf4e2
+
 
